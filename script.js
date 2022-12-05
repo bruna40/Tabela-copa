3a17ca38f8923bf255e116ce5381db21bec20a5c
Bruna Santiago <brunasanthiago@outlook.com>
2022-12-05 17:11:31 -0300

    ..

--- script.js
+++ script.js
@@ -8,7 +8,24 @@ const buttonSemi = document.querySelector('.butsemi');
 const semi = document.querySelector('.semiJogos');
 const buttonFinal = document.querySelector('.butfinal');
 const final = document.querySelector('.finalJogos');
+const escopo = document.querySelector('.escopo');
+const escopo1 = document.querySelector('.escopo1');
+const escopo2 = document.querySelector('.escopo2');
+const escopo3 = document.querySelector('.escopo3');
+const escopo4 = document.querySelector('.escopo4');
+const time = document.querySelector('.times');
+const escopoTime = document.querySelector('.timesCard');
 
+const escopoJogos = (xablau) => {
+    let html = '';
+    html += `
+        <tr> 
+            <th>Grupo</th>
+            <th>Jogo</th>
+        </tr>
+    `;
+    xablau.innerHTML = html;
+}
 const jogos = (data) => {
     let html = '';
     
@@ -30,13 +47,12 @@ const jogos = (data) => {
     
 }
 butFase1.addEventListener('click', () => {
+    escopoJogos(escopo);
 
     fetch('jogos-fase1.json')
         .then(response => response.json())
         .then(data => jogos(data));
 });
-
-
 
 const oito = (data) => {
     let html = '';
@@ -60,6 +76,7 @@ const oito = (data) => {
             
 
 button.addEventListener('click', () => {
+    escopoJogos(escopo1);
     fetch('oitavas.json')
         .then(response => response.json())
         .then(data => oito(data));
@@ -87,6 +104,7 @@ const quartasJogos = (data) => {
 }
 
 buttonQuarta.addEventListener('click', () => {
+    escopoJogos(escopo2);
     fetch('quartas.json')
         .then(response => response.json())
         .then(data => quartasJogos(data));
@@ -114,6 +132,7 @@ const semiJogos = (data) => {
 }
 
 buttonSemi.addEventListener('click', () => {
+    escopoJogos(escopo3);
     fetch('semi.json')
         .then(response => response.json())
         .then(data => semiJogos(data));
@@ -141,7 +160,37 @@ const finalJogos = (data) => {
 }
 
 buttonFinal.addEventListener('click', () => {
+    escopoJogos(escopo4);
     fetch('final.json')
         .then(response => response.json())
         .then(data => finalJogos(data));
 });
+
+
+const times = (data) => {
+    let html = '';
+
+    data.map(times => {
+        html += `
+            <div class="t">
+                <img class="imagem" src="assets/bandeiras/${times.img}" alt="${times.time}">
+                <div>
+                    <p>time: ${times.time}</p>
+                    <p>gols: ${times.gols}</p>
+                    <p>vitorias: ${times.vitorias}</p>
+                    <p>empates: ${times.empates}</p>
+                    <p>derrotas: ${times.derrotas}</p>
+                </div>
+            </div>
+        `;
+    });
+    escopoTime.innerHTML = html;
+
+};
+
+time.addEventListener('click', () => {
+    fetch('times.json')
+        .then(response => response.json())
+        .then(data => times(data));
+});
+
